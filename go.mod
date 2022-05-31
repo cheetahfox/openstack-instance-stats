42947b8f8912a40e0b6b2b2b9e3bccc499107176
Joshua Snyder <josh@cheetahfox.com>
2022-05-31 05:32:02 +0000

    Added Database writes, works in a quick test.

--- go.mod
+++ go.mod
@@ -5,4 +5,13 @@ go 1.17
 require (
 	github.com/davecgh/go-spew v1.1.1
 	github.com/gophercloud/gophercloud v0.24.0
+	github.com/influxdata/influxdb-client-go/v2 v2.9.0
+)
+
+require (
+	github.com/deepmap/oapi-codegen v1.8.2 // indirect
+	github.com/influxdata/line-protocol v0.0.0-20200327222509-2487e7298839 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -5,12 +5,16 @@ import (
 	"log"
 	"net"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
-	"github.com/davecgh/go-spew/spew"
 	"github.com/gophercloud/gophercloud"
 	"github.com/gophercloud/gophercloud/openstack"
 	"github.com/gophercloud/gophercloud/openstack/compute/v2/extensions/diagnostics"
 	"github.com/gophercloud/gophercloud/openstack/compute/v2/servers"
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/influxdata/influxdb-client-go/v2/api"
 )
 
 type openstackServers interface {
@@ -23,14 +27,15 @@ type vms struct {
 	Name      string
 	ProjectID string
 	IP        net.IP
-	Active    bool
+	Status    string
 }
 
 type sysconfig struct {
+	Bucket         string
 	InfluxdbServer string
-	InfluxDB       string
-	InfluxUsername string
-	InfluxPassword string
+	Org            string
+	Token          string
+	RefreshTime    int
 }
 
 // This fucntion sets up the program func startup() *gophercloud.ProviderClient
@@ -38,7 +43,7 @@ func startup() (*gophercloud.ProviderClient, sysconfig) {
 	var config sysconfig
 	var missingEnv bool
 
-	// Check that we have the require enviroment vars set
+	// Check that we have the require OpenStack enviroment vars set
 	if os.Getenv("OS_AUTH_URL") == "" {
 		log.Println("No Openstack Auth URL supplied")
 		missingEnv = true
@@ -86,7 +91,31 @@ func startup() (*gophercloud.ProviderClient, sysconfig) {
 		log.Println("No OpenStack Region Name supplied")
 		missingEnv = true
 	}
-	if missingEnv == true {
+	// Influx Enviromental Variables
+	if os.Getenv("INFLUX_SERVER") == "" {
+		log.Println("No Influxdb server specifed")
+		missingEnv = true
+	}
+	if os.Getenv("INFLUX_TOKEN") == "" {
+		log.Println("No Influxdb v2 Token specifed")
+		missingEnv = true
+	}
+	if os.Getenv("INFLUX_BUCKET") == "" {
+		log.Println("No Influx bucket specifed")
+		missingEnv = true
+	}
+	if os.Getenv("INFLUX_ORG") == "" {
+		log.Println("No Influx org specifed")
+		missingEnv = true
+	}
+	// Set the config from the Env
+	config.InfluxdbServer = os.Getenv("INFLUX_SERVER")
+	config.Token = os.Getenv("INFLUX_TOKEN")
+	config.Bucket = os.Getenv("INFLUX_BUCKET")
+	config.Org = os.Getenv("INFLUX_ORG")
+
+	// Log and exit if we are missing vars
+	if missingEnv {
 		log.Fatal("Missing Enviromental vars")
 	}
 
@@ -100,14 +129,11 @@ func startup() (*gophercloud.ProviderClient, sysconfig) {
 		log.Fatal(err)
 	}
 
+	// Just set the refresh time to 15 seconds for now.
+	config.RefreshTime = 15
+
 	return provider, config
 }
-
-/*
-func (servers []vms) updateServers(error) {
-
-}
-*/
 
 // Fill the server list for the first time
 func populateServers(provider *gophercloud.ProviderClient) ([]vms, error) {
@@ -140,9 +166,7 @@ func populateServers(provider *gophercloud.ProviderClient) ([]vms, error) {
 		s.UUID = server.ID
 		s.Name = server.Name
 		s.ProjectID = server.TenantID
-		if server.Status == "ACTIVE" {
-			s.Active = true
-		}
+		s.Status = server.Status
 
 		osServers = append(osServers, s)
 	}
@@ -165,34 +189,75 @@ func serverStats(provider *gophercloud.ProviderClient, serverId string) (map[str
 	return diags, nil
 }
 
+func statsWorker(config sysconfig, instances []vms, osProvider *gophercloud.ProviderClient, dbapi api.WriteAPI) {
+	ticker := time.NewTicker(time.Second * time.Duration(config.RefreshTime))
+	for range ticker.C {
+		for _, s := range instances {
+			if s.Status == "ACTIVE" {
+				stats, err := serverStats(osProvider, s.UUID)
+				if err != nil {
+					log.Println(err)
+					fmt.Println("Error while getting Server stats")
+				}
+				for k, v := range stats {
+					p := influxdb2.NewPointWithMeasurement("OpenStack Metrics").
+						AddTag("Instance Name", s.Name).
+						AddTag("UUID", s.UUID).
+						AddTag("Project", s.ProjectID).
+						AddField(k, v).
+						SetTime(time.Now())
+					dbapi.WritePoint(p)
+				}
+			}
+		}
+	}
+}
+
 func main() {
+	// Check the Enviromental Vars
 	osProvider, config := startup()
 
-	spew.Dump(config)
-
+	/*
+		Get the Instance list from openstack
+		In the future we will need to dynamically update this. But for now just pull the list at startup.
+	*/
 	osVms, err := populateServers(osProvider)
 	if err != nil {
 		log.Println(err)
 		log.Println("Error while populating server list")
 	}
 
-	for _, s := range osVms {
-		fmt.Println("-------------------------")
-		fmt.Printf("Server name: %s\n", s.Name)
-		fmt.Printf("Server UUID: %s\n", s.UUID)
-		fmt.Printf("Project ID: %s\n", s.ProjectID)
-		fmt.Println(s.Active)
-		stats, err := serverStats(osProvider, s.UUID)
-		if err != nil {
-			log.Println(err)
-			fmt.Println("Error while getting Server stats")
+	dbclient := influxdb2.NewClient(config.InfluxdbServer, config.Token)
+	writeAPI := dbclient.WriteAPI(config.Org, config.Bucket)
+	errorsCh := writeAPI.Errors()
+	// Catch any write errors
+	go func() {
+		for err := range errorsCh {
+			fmt.Printf("write error: %s\n", err.Error())
 		}
+	}()
 
-		spew.Dump(stats)
-	}
-	/*
-		opts := gophercloud.EndpointOpts{Region: os.Getenv("OS_REGION_NAME")}
+	// Go into the main loop.
+	go statsWorker(config, osVms, osProvider, writeAPI)
 
-		client, err := openstack.NewComputeV2(provider, opts)
-	*/
+	// Listen for Sigint or SigTerm and exit if you get them.
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		sig := <-sigs
+		fmt.Println()
+		fmt.Println(sig)
+		done <- true
+	}()
+
+	fmt.Println("Startup success")
+	<-done
+	// Close the Influxdb connection
+	writeAPI.Flush()
+	dbclient.Close()
+	fmt.Println("exiting")
+
 }
